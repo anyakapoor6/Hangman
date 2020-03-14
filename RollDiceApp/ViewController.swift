77e837c2693da40929469ec13711e77270bd0201
Anya Kapoor <anya@Anyas-MacBook-Air.local>
2020-03-13 19:34:33 -0700

    finish putting replit code into project

--- RollDiceApp/ViewController.swift
+++ RollDiceApp/ViewController.swift
@@ -10,17 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let names: [String] = [bean, cherry, maple,]
+    let words = ["batman", "geography", "dystopia", "circumlocution", "magnanimous", "usufruct", "coruscant", "varsity", "tennis", "spiders", "pythagorean", "trigonometry", "Washington", "Hamilton", "Calhoun", "Kentucky"]
+    var chosenWord: String?
+    var wordOnScreen: String?
+    
+   
+    
+//    func identifyLetter(w: String) {
+//        var word = String();
+//        var i = Int();
+//          for letters in chosenWord!{
+//            let ch = Array(wordOnScreen)[i];
+//            if String(letters) == w{
+//              word.append(letters)
+//            } else if (ch != "_")
+//            {
+//              word.append(ch);
+//            } else {
+//              word.append("_")
+//            }
+//            i=i+1;
+//          }
+//          wordOnScreen = word;
+//    }
+    
+
+    
     
     @IBOutlet weak var secretSantaLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        chosenWord = words.randomElement()!
+        wordOnScreen = String(repeating: "_", count: chosenWord!.count);
+        secretSantaLabel.text = wordOnScreen
         
+               
     }
     @IBAction func randomizer(_ sender: UIButton) {
-        secretSantaLabel.text = names.randomElement()!
+        secretSantaLabel.text = wordOnScreen
     }
     
 }
